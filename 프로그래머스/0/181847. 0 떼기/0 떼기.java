192364580f2618fafbc8413335525122153629ee
ghlim00 <156143969+ghlim00@users.noreply.github.com>
2025-06-06 17:40:56 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String n_str) {
+        
+        int answer = Integer.parseInt(n_str);
+        
+        String ans = Integer.toString(answer);
+        
+        return ans;
+        
+    }
+}
