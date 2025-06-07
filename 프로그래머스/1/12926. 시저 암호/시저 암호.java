78f7452a6ecfa7d772a6b79f934e763d6126ac2e
ghlim00 <156143969+ghlim00@users.noreply.github.com>
2025-06-08 01:27:55 +0900

    [level 1] Title: 시저 암호, Time: 1.47 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s, int n) {
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for(char c : s.toCharArray()){
+            if(c == ' '){
+                answer.append(' ');
+            }
+            else if(Character.isUpperCase(c)){
+                char shifted = (char)((c-'A'+n)%26 + 'A');
+                answer.append(shifted);
+            }
+            else if(Character.isLowerCase(c)){
+                char shifted = (char)((c-'a'+n)%26 + 'a');
+                answer.append(shifted);
+            }
+        }
+        
+        return answer.toString();
+        
+    }
+}
