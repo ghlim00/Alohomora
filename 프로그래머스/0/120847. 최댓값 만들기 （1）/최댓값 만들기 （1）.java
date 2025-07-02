b799ce5adc691a79d783b437c06a0282f1724b44
ghlim00 <156143969+ghlim00@users.noreply.github.com>
2025-07-02 23:16:06 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.50 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        
+        Arrays.sort(numbers);
+        
+        int a = numbers[numbers.length-1];
+        int b = numbers[numbers.length-2];
+        
+        return a*b;
+    }
+}
