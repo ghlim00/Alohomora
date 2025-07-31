6fcc6aba178c449cce98420daecdd1201c70efd0
ghlim00 <156143969+ghlim00@users.noreply.github.com>
2025-07-31 20:16:47 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.91 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numLog) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<numLog.length; i++){
+            
+            if((numLog[i] - numLog[i-1]) == 1 )
+                sb.append('w');
+            else if((numLog[i] - numLog[i-1]) == -1 )
+                sb.append('s');
+            else if((numLog[i] - numLog[i-1]) == 10 )
+                sb.append('d');
+            else if((numLog[i] - numLog[i-1]) == -10 )
+                sb.append('a');
+        }   
+        
+        return sb.toString();
+    }
+}
